8d40403b3a1577ce0395534f632eb528340faafc
Victor <victor.n.pisarev@gmail.com>
2025-12-22 15:59:38 +0300

    continious first launch

--- Utils/DateUtils.js
+++ Utils/DateUtils.js
@@ -1,10 +1,13 @@
 // utils/DateUtils.js
-class DateUtils {
-  static formatForAppSheet(date) {
-    return Utilities.formatDate(date, "GMT+3", "yyyy-MM-dd");
-  }
-  
-  static isValidDate(date) {
-    return date instanceof Date && !isNaN(date);
-  }
+class DateUtils 
+{
+    static formatForAppSheet(date)
+    {
+        return Utilities.formatDate(date, "GMT+3", "yyyy-MM-dd");
+    }
+    
+    static isValidDate(date) 
+    {
+        return date instanceof Date && !isNaN(date);
+    }
 }
